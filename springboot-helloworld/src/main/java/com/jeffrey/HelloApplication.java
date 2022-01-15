5abecaa35c44f2957dbf3abfdb17c6c13f44225b
yifei yang <jeffrey211314@gmail.com>
2022-01-15 10:12:55 -0800

    springboot-helloworld

---
+++ springboot-helloworld/src/main/java/com/jeffrey/HelloApplication.java
@@ -0,0 +1,14 @@
+package com.jeffrey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yifei yang
+ */
+@SpringBootApplication
+public class HelloApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloApplication.class, args);
+    }
+}
